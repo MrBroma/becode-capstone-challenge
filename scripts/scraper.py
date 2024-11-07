d409b6f92b0bef3763f141606e92f3411c081495
Loic Rouaud <loic.rouaud@gmail.com>
2024-11-07 11:51:33 +0100

    Restart of the project

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,50 +1,62 @@
 import requests
 import csv
+from typing import List
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from tqdm import tqdm
+
+def scrape_page(page: int, base_url: str) -> List[dict]:
+    url = f"{base_url}?_page={page}&_limit=100"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        articles = data.get('data', {}).get('articles', [])
+        return [
+            {
+                'id': article.get('id'),
+                'title': article.get('title'),
+                'category': article.get('dossierLabel'),
+                'date': article.get('publishedFrom'),
+                'link': f"https://www.rtbf.be/article/{article.get('slug')}-{article.get('id')}",
+                'summary': article.get('summary')
+            }
+            for article in articles
+        ]
+    else:
+        print(f"Failed to fetch page {page}: {response.status_code}")
+        return []
 
 def scrape_rtbf_data(base_url, pages, output_file):
     all_articles = []
-    root_url = "https://www.rtbf.be/article/"
     
-    # Loop through the specified number of pages
-    for page in range(1, pages + 1):
-        print(f"Scraping page {page}/{pages}...")
-        url = f"{base_url}?_page={page}&_limit=100"
-        response = requests.get(url)
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        futures = [executor.submit(scrape_page, page, base_url) for page in range(1, pages + 1)]
         
-        if response.status_code == 200:
-            data = response.json()
-            articles = data.get('data', {}).get('articles', [])
-            for article in articles:
-                article_id = article.get('id')
-                article_slug = article.get('slug')
-                article_url = f"{root_url}{article_slug}-{article_id}"
-                
-                article_data = {
-                    'id': article_id,
-                    'title': article.get('title'),
-                    'category': article.get('dossierLabel'),
-                    'date': article.get('publishedFrom'),
-                    'link': article_url
-                }
-                all_articles.append(article_data)
-        else:
-            print(f"Failed to fetch page {page}: {response.status_code}")
+        for future in tqdm(as_completed(futures), total=pages, unit='page'):
+            articles = future.result()
+            all_articles.extend(articles)
 
     # Write data to CSV
     with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['id', 'title', 'category', 'date', 'link']
+        fieldnames = ['id', 'title', 'summary', 'category', 'date', 'link']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for article in all_articles:
             writer.writerow(article)
 
     print(f"Scraping completed. Data saved to {output_file}")
+    return all_articles
 
-# Modify the variables as needed
-base_url = "https://bff-service.rtbf.be/oaos/v1.5/pages/en-continu"
-pages_to_scrape = 50  # Change this to scrape more or fewer pages
-output_csv = "rtbf_articles.csv"
+def get_top_articles(n: int) -> List[dict]:
+    """Récupère les n articles les plus récents"""
+    base_url = "https://bff-service.rtbf.be/oaos/v1.5/pages/en-continu"
+    pages_to_scrape = n // 100 + 1
+    articles = scrape_rtbf_data(base_url, pages_to_scrape, "data/rtbf_articles.csv")
+    return articles[:n]
 
-scrape_rtbf_data(base_url, pages_to_scrape, output_csv)
-
-
+if __name__ == "__main__":
+    # Modify the variables as needed
+    base_url = "https://bff-service.rtbf.be/oaos/v1.5/pages/en-continu"
+    pages_to_scrape = 100  # Change this to scrape more or fewer pages
+    output_csv = "data/rtbf_articles.csv"
+    scrape_rtbf_data(base_url, pages_to_scrape, output_csv)
